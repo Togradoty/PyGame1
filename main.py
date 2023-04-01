8f2fe0e0a6a048450e556c94711125c8c89fe2cb
Togradoty <togradoty@gmail.com>
2023-03-31 20:42:46 -0500

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import time
+e = 1
+#making a purple window
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    #pygame.QUIT means user clicked X
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #clears screen and fills with purple
+    screen.fill("purple")
+    #flip screen to put work on screen
+    pygame.display.flip()
+
+    clock.tick(60) #limits fps to 60
+
+pygame.quit()
